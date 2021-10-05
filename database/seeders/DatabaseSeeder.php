806eb995f79fc2da185fd4a24e3d9bb054d95c74
Will Percey <willpercey@googlemail.com>
2021-10-05 20:00:54 +0100

    Call Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Repositories\PhonebookRepository;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(SystemSeeder::class);
+        $this->call(DemoSeeder::class);
     }
 }
